78c545110224741f2af06aa595514e63c7080629
Lucas Diogo <luc-diogo@outlook.com>
2019-08-08 17:57:42 -0300

    confirm

--- lib/Models/Core/Config.php
+++ lib/Models/Core/Config.php
@@ -4,7 +4,7 @@ namespace Models\Core;
 
 class Config
 {
-    private static $Token = "5A3A044DE838403F9566BDFBEE9DE763";
+    private static $Token = "x-api-key";
     private static $IsSandBox = true;
 
     public static function getToken()

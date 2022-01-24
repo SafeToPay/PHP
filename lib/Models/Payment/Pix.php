6771650ea69d4549aea52177872ce72101d3bbb5
Franciel Dors <frankdors@users.noreply.github.com>
2022-01-24 18:03:32 -0300

    Faltava JsonSerializable no \Safe2Pay\Models\Payment\Pix

--- lib/Models/Payment/Pix.php
+++ lib/Models/Payment/Pix.php
@@ -7,18 +7,26 @@ namespace Safe2Pay\Models\Payment;
  *
  * @package Safe2Pay\Models
  */
-class Pix
+class Pix implements \JsonSerializable
 {
+
     private $Expiration;
 
     public function getExpiration()
-	{
-		return $this->Expiration;
-	}
+    {
+        return $this->Expiration;
+    }
 
-	public function setExpiration($Expiration)
-	{
-		$this->Expiration = $Expiration;
-	}
+    public function setExpiration($Expiration)
+    {
+        $this->Expiration = $Expiration;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            "Expiration" => $this->Expiration
+        ];
+    }
 
 }
